4b7390efc2fb1fad48c18a7676aa922f3dbea9a7
public-release <as-public-release@janestreet.com>
2026-05-18 12:36:24 +0100

    v0.18~preview.130.100+614

--- src/time_now_stubs.c
+++ src/time_now_stubs.c
@@ -22,6 +22,8 @@ CAMLprim value time_now_nanoseconds_since_unix_epoch_or_zero() {
 
 #else
 
+#ifndef _MSC_VER
+
 #include <sys/time.h>
 #include <sys/types.h>
 
@@ -34,6 +36,37 @@ CAMLprim value time_now_nanoseconds_since_unix_epoch_or_zero() {
                             (uint64_t)tp.tv_usec * 1000);
 }
 
+#else /* compiling with cl */
+
+#if _MSC_VER < 1900 /* timespec_get is supported starting VS 2015 */
+
+/*
+ * Note that there are fallback implementations of gettimeofday for Windows.
+ * I am not convinced there is much value in supporting Visual Studio versions
+ * older than 2015 in an OCaml library, so we just croak here instead of
+ * falling back.
+ */
+
+#error timespec_get is only available in Visual Studio 2015 (14.0) and later
+#endif
+
+#include <time.h>
+
+/*
+ * https://en.cppreference.com/w/c/chrono/timespec_get
+ * https://docs.microsoft.com/en-us/cpp/c-runtime-library/reference/timespec-get-timespec32-get-timespec64-get1?view=msvc-160
+ */
+
+CAMLprim value time_now_nanoseconds_since_unix_epoch_or_zero() {
+  struct timespec ts;
+
+  if (timespec_get(&ts, TIME_UTC) != TIME_UTC)
+    return caml_alloc_int63(0);
+  else
+    return caml_alloc_int63(NANOS_PER_SECOND * (uint64_t)ts.tv_sec +
+                            (uint64_t)ts.tv_nsec);
+}
+#endif
 #endif
 
 CAMLprim value time_now_nanosecond_counter_for_timing() {
